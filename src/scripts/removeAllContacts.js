ce3cfd627562c9a60f44def31e9d63c16fa98b3f
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-08-06 18:04:36 +0200

    removeAll, removeLast implemented

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,8 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from "node:fs/promises";
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+    fs.writeFile(PATH_DB, "[]");
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from "node:fs/promises";
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+    fs.readFile(PATH_DB, { encoding: "UTF-8" })
+        .then((data) => JSON.parse(data))
+        .then((contacts) => {
+            contacts.pop();
+            fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+        })
+        .catch((error) => console.log("error: ", error));
+};
 
 removeLastContact();
