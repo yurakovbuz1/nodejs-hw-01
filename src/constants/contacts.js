17273eda77a56e25f1019a89428827abd7d81567
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-08-06 17:33:53 +0200

    generate implemented

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import path from "node:path";
+
+export const PATH_DB = path.resolve("./src/db/db.json");
+
+console.log(PATH_DB);
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import * as fs from "node:fs/promises";
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
 
-generateContacts(5);
+    fs.readFile(PATH_DB, { encoding: "UTF-8" })
+        .then((data) => JSON.parse(data))
+        .then((contacts) => {
+            for (let i = 0; i < number; i++) {
+                contacts.push(createFakeContact());
+            }
+            console.log(contacts);
+            fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+        });
+};
+
+generateContacts(3);
